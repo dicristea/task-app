d087801140b6b9a57031367fa6195079f3e1c08b
“dicristea” <dicristea111@gmail.com>
2022-09-28 21:35:34 -0700

    list renders, use state.task.number instead of <ol>

---
+++ src/components/Overview.js
@@ -0,0 +1,17 @@
+//Will display/render all our tasks
+
+import React from "react";
+
+const Overview = (props) => {
+    const { tasks } = props; // takes tasks from the props
+
+    return(
+        <ul>
+            {tasks.map((task) => {
+                return <li key={task.id}>{task.number}. {task.text}</li>;
+            })}
+        </ul>
+    );
+};
+
+export default Overview;
